1b80715c57d996641d85bddc8824e3933e132dcb
ShakaNash <funyarn@funyarndemacbook-pro.local>
2016-03-19 12:18:44 +0800

    Initial Commit

---
+++ WhatEver/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WhatEver
+//
+//  Created by Funyarn on 16/3/19.
+//  Copyright © 2016年 Funyarn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ WhatEverTests/WhatEverTests.m
@@ -0,0 +1,39 @@
+//
+//  WhatEverTests.m
+//  WhatEverTests
+//
+//  Created by Funyarn on 16/3/19.
+//  Copyright © 2016年 Funyarn. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WhatEverTests : XCTestCase
+
+@end
+
+@implementation WhatEverTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
